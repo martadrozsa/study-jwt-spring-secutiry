0438ac90b40c345998614052ec519020017f33ec
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-20 15:33:55 -0300

    Project created

---
+++ src/main/java/com/martadrozsa/studyjwtspringsecutiry/StudyJwtSpringSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.martadrozsa.studyjwtspringsecutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJwtSpringSecutiryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJwtSpringSecutiryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martadrozsa/studyjwtspringsecutiry/StudyJwtSpringSecutiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martadrozsa.studyjwtspringsecutiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJwtSpringSecutiryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
